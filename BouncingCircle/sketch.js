749acc5e20e088adc990f443249b8d11956101fc
hzamani2 <hmojtabaee-zamani@packer.edu>
2017-10-27 10:15:11 -0400

    update

--- BouncingCircle/sketch.js
+++ BouncingCircle/sketch.js
@@ -1,7 +1,29 @@
+var x = 0;
+var y = 0;
+var xspeed = 3;
+var yspeed = 2;
+
 function setup() {
-  // put setup code here
+	createCanvas (windowWidth, windowHeight)
 }
 
 function draw() {
-  // put drawing code here
+	background (255);
+	fill (0, 0, 255);
+	ellipse (x, y, 50, 50);
+	x = x + xspeed;
+	y = y + yspeed;
+	if (x > width) {
+		xspeed *= -1;
+	}
+	if (y > height) {
+		yspeed *= -1;
+	}
+	if (x < 0) {
+		xspeed *= -1;
+	}
+	if (y < 0) {
+		yspeed *= -1;
+	}
+
 }
